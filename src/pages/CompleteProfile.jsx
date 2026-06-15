bcb280d8d076edbe6a30dd85798eb6a8dd9d1f8b
Ashutosh Rout <ashutoshbiki786@gmail.com>
2026-06-15 03:22:30 -0700

    Fix FormField import

--- src/pages/CompleteProfile.jsx
+++ src/pages/CompleteProfile.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ImagePlus } from "lucide-react";
-import { FormField } from "../components/ui/FormField";
+import { FormField } from "../components/ui/formfield";
 import { PrimaryButton } from "../components/ui/PrimaryButton";
 import { profileupdate } from "@/services/allServices";
 import { useApp } from "../lib/app-store";
