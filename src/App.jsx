42787813fad33e14cf5eecda6dbd36ef65e05def
Ashutosh Rout <ashutoshbiki786@gmail.com>
2026-07-17 08:17:11 -0700

    remove cat

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,13 @@ import PaymentSuccess from "./pages/PaymentSuccess";
 import VerifyEmail from "./pages/VerifyEmail";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
-import CatCursor from "./components/CatCursor";
+
 
 
 function App() {
   return (
     <MainLayout>
       <Toaster richColors position="top-right" />
-      <CatCursor /> 
       <Routes>
         {/* Public Routes */}
         <Route path="/" element={<Home />} />
